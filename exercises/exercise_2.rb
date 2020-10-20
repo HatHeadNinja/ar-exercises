19293d861c829e83497fa3e8f85b066db672487b
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-20 10:42:23 -0700

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(name: "Burnaby")
+@store2 = Store.find_by(name: "Richmond")
+puts @store1.annual_revenue
+
+@store1.name = "Metrotown"
+puts @store1.name
+puts @store2.name
